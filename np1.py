89de63293182ae0afd966167ac32d93377be6045
deepak0642 <deepakyadav0642@gmail.com>
2026-04-25 00:11:22 +0530

    learnt numpy till slicing

--- np1.py
+++ np1.py
@@ -3,3 +3,27 @@ array  = np.array([1  ,3 ,4 ,5])
 print(array)
 array = array * 2
 print(array)
+
+#2d  , 3d 4d 
+
+narray = np.array([[['A' , 'B ', 'C' , 'D'] , ['A' , 'B ', 'C' , 'D'] ,['A' , 'B ', 'C' , 'D']],
+                    [['A' , 'B ', 'C' , 'D'] ,['A' , 'B ', 'C' , 'D'] ,['A' , 'B ', 'C' , 'D']],
+                    [['A' , 'B ', 'C' , 'D'] ,['A' , 'B ', 'C' , 'D'] ,['A' , 'B ', 'C' , 'D']]])
+
+word  = narray[2 , 0 ,1]
+print(word)
+print(narray.shape)
+
+#slicing
+
+array = np.array([[1, 2, 3, 4],
+                  [5 ,6 ,7, 8],
+                  [6 , 9 , 3, 5]])
+
+print(array[2])
+print(array[: , 2])
+print(array[:  , 1:4:2])
+#if step is negative then it is reversed
+print(array[0:2 , 0:2])
+
+
