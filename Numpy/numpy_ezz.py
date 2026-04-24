f55492459616f2b42c250de32352afb27138efc6
deepak0642 <deepakyadav0642@gmail.com>
2026-04-24 23:50:55 +0530

    fixed enviromental variable issue and also we cant have a file same name as a library

--- Numpy/numpy_ezz.py
+++
@@ -1,5 +0,0 @@
-import numpy_ezz as np
-array = np.array([1 , 2, 2 ,3])
-print(array)
-array = array * 2
-print(array)
---
+++ np1.py
@@ -0,0 +1,5 @@
+import numpy as np
+array  = np.array([1  ,3 ,4 ,5])
+print(array)
+array = array * 2
+print(array)
