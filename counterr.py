04d0ff85dffee8d531d02c43a97cc251a4e4309d
deepak0642 <deepakyadav0642@gmail.com>
2026-04-30 23:46:57 +0530

    read csv file and plotted bar chart

--- counterr.py
+++ counterr.py
@@ -1,12 +1,24 @@
 from collections import Counter as cs
-c = cs([ 'p' , 'a' , 'd'])
+from matplotlib import pyplot as plt
+import pandas as pd
+c= cs()
+data = pd.read_csv('data.csv')
+l = data['LanguagesWorkedWith']
+for items  in l:
+    c.update(items.split(';'))
+c= c.most_common(15)
 print(c)
-c.update(['p' , 'b' , 'd'])
-print(c)
-print(c.most_common(1))
-#will print most common as a tuple
-d = []
-for item  in c.most_common(1) :
-    d.append(item[0])
 
-print(d)
+language =[]
+popularity = []
+for i in c:
+    language.append(i[0])
+    popularity.append(i[1])
+
+plt.barh(language , popularity )#, width= 0.25  )
+plt.title('kuch to kia h')
+plt.savefig('bar.png')
+plt.show()
+
+
+
