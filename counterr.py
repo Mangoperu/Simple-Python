f1496059888efd018212af7c9b9d821516cd768a
deepak0642 <deepakyadav0642@gmail.com>
2026-04-30 23:20:50 +0530

    learnt some data handling

--- counterr.py
+++ counterr.py
@@ -3,3 +3,10 @@ c = cs([ 'p' , 'a' , 'd'])
 print(c)
 c.update(['p' , 'b' , 'd'])
 print(c)
+print(c.most_common(1))
+#will print most common as a tuple
+d = []
+for item  in c.most_common(1) :
+    d.append(item[0])
+
+print(d)
